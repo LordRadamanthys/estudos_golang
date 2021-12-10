cfbf5c8f78bc5b0558035d655a699548ee4e2eb5
lordradamanthys <mateuslima565@gmail.com>
2021-12-09 22:12:03 -0300

    system pakages

---
+++ pacotes/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/lordRadamanthys/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(6.0, 5.6))
+	fmt.Println(area.TrianguloEq(6.2, 3.3))
+}
---
+++ pacotes/usandolib/usandoLib.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/LordRadamanthys/got_area"
+)
+
+func main() {
+	fmt.Println(got_area.Circ(2.0))
+	fmt.Println(got_area.Rect(2.0, 4.0))
+	fmt.Println(got_area.TrianguloEq(2.0, 2))
+}
