637d75a1d0b93e3a04edeb83ded0d6d4dc5a7a7b
lordradamanthys <mateuslima565@gmail.com>
2021-12-12 00:04:55 -0300

    loop in channel and go routines encapsulated

--- concorrencia/buffer/buffer.go
+++ concorrencia/buffer/buffer.go
@@ -9,16 +9,16 @@ func rotina(ch chan int) {
 	ch <- 1
 	ch <- 2
 	ch <- 3
-	fmt.Println("Executou")
 	ch <- 4
 	ch <- 5
 	ch <- 6
+	fmt.Println("Executou")
 }
 
 func main() {
 	ch := make(chan int, 3)
 
 	go rotina(ch)
-	time.Sleep(time.Second)
+	time.Sleep(time.Second * 5)
 	fmt.Println(<-ch)
 }
